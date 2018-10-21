b8dc52e5eb47b0abdddfe4d372728b441519cd32
Pau Andrio <andriopau@gmail.com>
2018-10-21 20:06:58 +0200

    Adding setup.py for Pypi

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="biobb_io",
-    version="0.0.8",
+    name="biobb_model",
+    version="0.0.1",
     author="Biobb developers",
     author_email="pau.andrio@bsc.es",
-    description="Biobb_io is the Biobb module collection to fetch data to be consumed by the rest of the Biobb building blocks.",
+    description="biobb_model is the Biobb module collection to fetch data to be consumed by the rest of the Biobb building blocks.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords="Bioinformatics Workflows BioExcel Compatibility",
-    url="https://github.com/bioexcel/biobb_io",
+    url="https://github.com/bioexcel/biobb_model",
     project_urls={
-        "Documentation": "http://biobb_io.readthedocs.io/en/latest/",
+        "Documentation": "http://biobb_model.readthedocs.io/en/latest/",
         "Bioexcel": "https://bioexcel.eu/"
     },
-    packages=setuptools.find_packages(exclude=['docs', 'test', 'cwl','notebooks',]),
-    install_requires=['mistune==0.7.4', 'jsonschema==2.6.0', 'biobb_common', 'requests', ],
+    packages=setuptools.find_packages(exclude=['docs', 'test', 'cwl', 'notebooks',]),
+    install_requires=['mistune==0.7.4', 'jsonschema==2.6.0', 'biobb_common', 'biopython' ],
     python_requires='>=3.5',
     classifiers=(
         "Development Status :: 3 - Alpha",
