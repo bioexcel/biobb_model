ca077557d9c52953ad802708ad7eaa8d217933e9
Pau Andrio <andriopau@gmail.com>
2019-11-28 14:36:56 +0100

    Adding launch logger decorator

--- biobb_model/model/mutate.py
+++ biobb_model/model/mutate.py
@@ -2,34 +2,38 @@
 
 """Module containing the Mutate class and the command line interface."""
 import argparse
-from biobb_common.configuration   import settings
+from biobb_common.configuration import settings
 from biobb_common.tools import file_utils as fu
 from biobb_common.command_wrapper import cmd_wrapper
+from biobb_common.tools.file_utils import launchlogger
 
-class Mutate():
+
+class Mutate:
     """Class to mutate one aminoacid by another in a 3d structure.
 
     Args:
         input_pdb_path (str): Input PDB file path.
         output_pdb_path (str): Output PDB file path.
         properties (dic):
-            | - **mutation_list** (*str*): ("A:Val2Ala") Mutation list in the format "Chain:WT_AA_ThreeLeterCode Resnum MUT_AA_ThreeLeterCode" (no spaces between the elements) separated by commas. If no chain is provided as chain code all the chains in the pdb file will be mutated. ie: "A:ALA15CYS"
-            | - **remove_tmp** (*bool*) - (True) [WF property] Remove temporal files.
-            | - **restart** (*bool*) - (False) [WF property] Do not execute if output files exist.
+            * **mutation_list** (*str*): ("A:Val2Ala") Mutation list in the format "Chain:WT_AA_ThreeLeterCode Resnum MUT_AA_ThreeLeterCode" (no spaces between the elements) separated by commas. If no chain is provided as chain code all the chains in the pdb file will be mutated. ie: "A:ALA15CYS"
+            * **remove_tmp** (*bool*) - (True) [WF property] Remove temporal files.
+            * **restart** (*bool*) - (False) [WF property] Do not execute if output files exist.
     """
 
     def __init__(self, input_pdb_path, output_pdb_path, properties=None, **kwargs):
         properties = properties or {}
 
         # Input/Output files
-        self.input_pdb_path = input_pdb_path
-        self.output_pdb_path = output_pdb_path
+        self.io_dict = {
+            "in": {"input_pdb_path": input_pdb_path},
+            "out": {"output_pdb_path": output_pdb_path}
+        }
 
         # Properties specific for BB
         self.check_structure_path = properties.get('check_structure_path', 'check_structure')
-        self.mutation_list = properties.get('mutation_list', 'A:Val2Ala').replace(" ","")
+        self.mutation_list = properties.get('mutation_list', 'A:Val2Ala').replace(" ", "")
 
-        # Common in all BB
+        # Properties common in all BB
         self.can_write_console_log = properties.get('can_write_console_log', True)
         self.global_log = properties.get('global_log', None)
         self.prefix = properties.get('prefix', None)
@@ -41,44 +45,45 @@ class Mutate():
         # Check the properties
         fu.check_properties(self, properties)
 
+    @launchlogger
     def launch(self):
         """Mutate one or more aminoacids."""
         tmp_files = []
 
-        #Create local logs
-        out_log, err_log = fu.get_logs(path=self.path, prefix=self.prefix, step=self.step, can_write_console=self.can_write_console_log)
+        # Get local loggers from launchlogger decorator
+        out_log = getattr(self, 'out_log', None)
+        err_log = getattr(self, 'err_log', None)
 
-        #Restart if needed
+        # Restart if needed
         if self.restart:
-            output_file_list = [self.output_pdb_path]
-            if fu.check_complete_files(output_file_list):
+            if fu.check_complete_files(self.io_dict["out"].values()):
                 fu.log('Restart is enabled, this step: %s will the skipped' % self.step, out_log, self.global_log)
                 return 0
 
         cmd = [self.check_structure_path,
-               '-i', self.input_pdb_path,
-               '-o', self.output_pdb_path,
+               '-i', self.io_dict["in"]["input_pdb_path"],
+               '-o', self.io_dict["out"]["output_pdb_path"],
                'mutateside', '--mut', self.mutation_list]
 
         returncode = cmd_wrapper.CmdWrapper(cmd, out_log, err_log, self.global_log).launch()
 
         if self.remove_tmp:
-            fu.rm_file_list(tmp_files)
+            fu.rm_file_list(tmp_files, out_log=out_log)
 
         return returncode
 
-def main():
-    """Command line interface."""
-    parser = argparse.ArgumentParser(description="Model the missing atoms in aminoacid side chains of a PDB.", formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=99999))
-    parser.add_argument('-c', '--config', required=False, help="This file can be a YAML file, JSON file or JSON string")
-    parser.add_argument('--system', required=False, help="Check 'https://biobb-common.readthedocs.io/en/latest/system_step.html' for help")
-    parser.add_argument('--step', required=False, help="Check 'https://biobb-common.readthedocs.io/en/latest/system_step.html' for help")
 
-    #Specific args of each building block
+def main():
+    parser = argparse.ArgumentParser(description="Model the missing atoms in aminoacid side chains of a PDB.",
+                                     formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=99999))
+    parser.add_argument('-c', '--config', required=False, help="This file can be a YAML file, JSON file or JSON string")
+    parser.add_argument('--system', required=False, help="Common name for workflow properties set")
+    parser.add_argument('--step', required=False, help="Check 'https://biobb-common.readthedocs.io/en/latest/configuration.html")
+
+    # Specific args of each building block
     required_args = parser.add_argument_group('required arguments')
     required_args.add_argument('-i', '--input_pdb_path', required=True, help="Input PDB file name")
     required_args.add_argument('-o', '--output_pdb_path', required=True, help="Output PDB file name")
-    ####
 
     args = parser.parse_args()
     config = args.config if args.config else None
@@ -86,8 +91,9 @@ def main():
     if args.step:
         properties = properties[args.step]
 
-    #Specific call of each building block
+    # Specific call of each building block
     Mutate(input_pdb_path=args.input_pdb_path, output_pdb_path=args.output_pdb_path, properties=properties).launch()
+
 
 if __name__ == '__main__':
     main()
