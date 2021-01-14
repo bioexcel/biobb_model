578cb816d7e72ee51e84599ce4e429efb443152e
Pau Andrio <andriopau@gmail.com>
2021-01-14 16:08:18 +0100

    fix(fix_side_chain): Call to standalone function in test.

--- biobb_model/test/unitests/test_model/test_fix_side_chain.py
+++ biobb_model/test/unitests/test_model/test_fix_side_chain.py
@@ -1,14 +1,16 @@
 from biobb_common.tools import test_fixtures as fx
-from biobb_model.model.fix_side_chain import FixSideChain
+from biobb_model.model.fix_side_chain import fix_side_chain
+
 
 class TestFixSideChain:
     def setUp(self):
         fx.test_setup(self, 'fix_side_chain')
 
     def tearDown(self):
-        fx.test_teardown(self)
+        #fx.test_teardown(self)
+        pass
 
     def test_launch(self):
-        FixSideChain(properties=self.properties, **self.paths).launch()
+        fix_side_chain(properties=self.properties, **self.paths)
         assert fx.not_empty(self.paths['output_pdb_path'])
         assert fx.equal(self.paths['output_pdb_path'], self.paths['reference_output_pdb_path'])
