73975134e78819377b48cc43c2948cc89fe2ed21
Pau Andrio <andriopau@gmail.com>
2024-11-21 11:28:47 +0100

    [CI/CD](test_checking_log): Update test reference file checking.log.

--- biobb_model/test/unitests/test_model/test_checking_log.py
+++ biobb_model/test/unitests/test_model/test_checking_log.py
@@ -1,17 +1,22 @@
 # type: ignore
 from biobb_common.tools import test_fixtures as fx
+
 from biobb_model.model.checking_log import checking_log
 
 
 class TestCheckingLog:
     def setup_class(self):
-        fx.test_setup(self, 'checking_log')
+        fx.test_setup(self, "checking_log")
 
     def teardown_class(self):
         fx.test_teardown(self)
-        #  pass
+        # pass
 
     def test_launch(self):
         checking_log(properties=self.properties, **self.paths)
-        assert fx.not_empty(self.paths['output_log_path'])
-        assert fx.equal(self.paths['output_log_path'], self.paths['reference_output_log_path'], ignore_list=['loaded', '=    '])
+        assert fx.not_empty(self.paths["output_log_path"])
+        assert fx.equal(
+            self.paths["output_log_path"],
+            self.paths["reference_output_log_path"],
+            ignore_list=["loaded", "=    "],
+        )
